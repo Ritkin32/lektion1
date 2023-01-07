6e5d48e5ff1f5820fc9f723479076baf97a6f91c
rita <AdaRitDN@mail.ru>
2023-01-08 00:25:56 +0300

    папк6

---
+++ exzample006/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string uzername = Console Readline();
+
+if (uzername =="Маша");
+{
+     Console.WriteLine("Ура, это же МАША");
+}     
+else
+{
+      Console.Write("Привет,");
+      Console.WriteLine (uzername);
+}
+
+
