836d5ed12d5aebddceb05f864affeae295323fa1
rita <AdaRitDN@mail.ru>
2023-01-08 00:46:35 +0300

    123

--- exzample006/Program.cs
+++ exzample006/Program.cs
@@ -1,14 +1,15 @@
 ﻿Console.WriteLine("Введите имя пользователя:");
 string uzername = Console Readline();
 
-if (uzername =="Маша");
+if (uzername =="Маша")
 {
-     Console.WriteLine("Ура, это же МАША");
+    Console.WriteLine("Ура, это же МАША");
 }     
 else
 {
-      Console.Write("Привет,");
-      Console.WriteLine (uzername);
+    Console.Write("Привет,");
+    Сonsole.WriteLine (uzername);
 }
+;
 
 
