d76b811b9a7f75ad14e74597905974faa860c252
rita <AdaRitDN@mail.ru>
2023-01-08 19:39:13 +0300

    массив2

---
+++ exz13/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 15, 18, 44 ,57, 29, 37, 39};
+
+int n = array.Length;
+int find = 44;
+
+int index = 0;
+
+while (index < n)
+{
+  if(array[index] == find)
+  break;
+  {
+    Console.WriteLine(index);
+  }
+  index++;
+}
