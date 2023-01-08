b8ea750e159561625b7c577311655072aa3ffe23
rita <AdaRitDN@mail.ru>
2023-01-08 12:09:10 +0300

    макс

---
+++ exz9/Program.cs
@@ -0,0 +1,22 @@
+﻿int a1 = 12;
+int a2 = 13;
+int a3 = 14;
+int b1 = 70;
+int b2 = 458;
+int b3 = 17;
+int c1 = 40;
+int c2 = 19;
+int c3 = 20;
+
+int max = a1;
+if(b1> max ) max = b1;
+if(b2> max ) max = b2;
+if(b3> max ) max = b3;
+if(a2> max ) max = a2;
+if(a3> max ) max = a3;
+if(c1> max ) max = c1;
+if(c2> max ) max = c2;
+if(c3> max ) max = c3;
+
+Console.WriteLine(max);
+
